552f07fae96978f07a12d5a2e9e381c3e6ae38da
Usman Ahmed (02962) <usman.ahmed@nxb.com.pk>
2023-03-15 12:34:54 +0500

    10 push

--- src/config2.js
+++ src/config2.js
@@ -1,5 +1,5 @@
-// const API_URL2 = "http://localhost:8000";
-const API_URL2 = "https://backend.growthtracker.vteamslabs.com/public";
+const API_URL2 = "http://localhost:8000";
+// const API_URL2 = "https://backend.growthtracker.vteamslabs.com/public";
 
 
 export default API_URL2;
